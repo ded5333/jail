44fb34b2d9c7cdbb55a9179c4be37a9d86efd3e5
ded <ded5333@inbox.ru>
2021-06-21 17:09:58 +0300

    Начало разроботки теста (Прописка на зоне)

---
+++ app/src/main/java/com/ded/android/myapplicationjail/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ded.android.myapplicationjail;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  NavController navController;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    navController = Navigation.findNavController(this, R.id.navigation);
+
+
+  }
+}
